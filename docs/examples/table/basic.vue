b2c2867b68207fb1149f7b0a02c644dd3c97f917
1464035769chen <105584799+1464035769chen@users.noreply.github.com>
2024-08-16 16:54:55 +0800

    Update basic.vue

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -7,26 +7,32 @@
 </template>
 
 <script lang="ts" setup>
+const ref=(data);
+  
 const tableData = [
   {
     date: '2016-05-03',
     name: 'Tom',
     address: 'No. 189, Grove St, Los Angeles',
+    date:''
   },
   {
     date: '2016-05-02',
     name: 'Tom',
     address: 'No. 189, Grove St, Los Angeles',
+    date:''
   },
   {
     date: '2016-05-04',
     name: 'Tom',
     address: 'No. 189, Grove St, Los Angeles',
+    date:''
   },
   {
     date: '2016-05-01',
     name: 'Tom',
     address: 'No. 189, Grove St, Los Angeles',
+    date:''
   },
 ]
 </script>
